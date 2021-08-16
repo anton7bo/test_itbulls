0d1f38cdd7377f2841ef00b29a4907ef3aa326ed
An <zksn8z@gmail.com>
2021-08-16 20:25:27 +0500

    updated first commit

---
+++ test.pas
@@ -0,0 +1,19 @@
+unit test;
+
+interface
+type
+  TClass1 = object
+    private
+      Fld1: integer;
+    public
+      procedure Test1;
+  end
+
+implementation
+
+procedure TClass1.Test1;
+begin
+  Process;
+end;
+
+end
